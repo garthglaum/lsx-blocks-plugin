b3c7f4151814d5e4bdbc2192acd06eecc373737c
Warwick Booth <warwick@lsdev.biz>
2019-05-21 08:37:48 +0200

    Adding in the first version of the files

---
+++ lsx-blocks-plugin.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Plugin Name: LSX Blocks Plugin
+ * Plugin URI:  https://github.com/lightspeeddevelopment/lsx-blocks-plugin
+ * Description:	
+ * Version:     1.0.0
+ * Author:      LightSpeed
+ * Author URI:  https://www.lsdev.biz/
+ * License:     GPL3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: lsx-blocks-plugin
+ * Domain Path: /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define( 'LSX_BLOCKS_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+define( 'LSX_BLOCKS_PLUGIN_CORE', __FILE__ );
+define( 'LSX_BLOCKS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'LSX_BLOCKS_PLUGIN_VER', '1.2.3' );
+
+
+/* ======================= Below is the Plugin Class init ========================= */
+
+//require_once( LSX_BLOCKS_PLUGIN_PATH . 'classes/class-core.php' );
+
---
+++ readme.txt
@@ -0,0 +1,23 @@
+=== LSX Blocks Plugin ===
+Contributors: feedmymedia
+Donate link: https://www.donate.lsdev.biz/
+Tags: LSX Theme, Gutenberg, Blog Customizer, WordPress Customizer, WordPress Blog
+Requires at least: 4.3
+Tested up to: 5.1.1
+Requires PHP: 7.0
+Stable tag: 1.2.3
+License: GPLv3
+
+
+== Description ==
+
+= Works with the LSX Theme =
+
+= Support =
+We offer premium support for this plugin. Premium support that can be purchased via [lsdev.biz](https://www.lsdev.biz/services/support/).
+
+== Frequently Asked Questions ==
+
+== Changelog ==
+
+
