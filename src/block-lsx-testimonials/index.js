c69a1cbce28f6bd4d9525952d862fb8a40e99c8a
JosephHart <joe@lsdev.biz>
2019-05-23 14:16:05 +0200

    Post carousel block registering

--- src/block-lsx-testimonials/index.js
+++ src/block-lsx-testimonials/index.js
@@ -28,14 +28,13 @@ const validAlignments = [ 'center', 'wide' ];
 export const name = 'core/latest-posts';
 
 // Register the block
-registerBlockType( 'lsx-blocks/lsx-post-carousel', {
-	title: __( 'LSX Post Carousel', 'lsx-blocks' ),
-	description: __( 'Add a carousel or list of customizable posts to your page.', 'lsx-blocks' ),
+registerBlockType( 'lsx-blocks/block-lsx-testimonials', {
+	title: __( 'LSX Testimonials', 'lsx-blocks' ),
+	description: __( 'Add a carousel or list of your testimonials', 'lsx-blocks' ),
 	icon: 'images-alt2',
 	category: 'lsx-blocks',
 	keywords: [
-		__( 'post', 'lsx-blocks' ),
-		__( 'carousel', 'lsx-blocks' ),
+		__( 'testimonial', 'lsx-blocks' ),
 		__( 'lsx', 'lsx-blocks' ),
 	],
 
