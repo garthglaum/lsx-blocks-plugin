17ae053be22cb2ca3476a62d8d9159aded1192d0
JosephHart <joe@lsdev.biz>
2019-05-23 13:16:50 +0200

    Adding filter for the testimonial block mobile content and source tag

--- includes/classes/class-frontend.php
+++ includes/classes/class-frontend.php
@@ -21,6 +21,7 @@ class Frontend {
 		 * Contructor
 		 */
 		public function __construct() {
+			add_filter( 'the_content', array( $this, 'mobile_srcset_tag' ), 10, 1 );
 		}
 
 		/**
@@ -38,5 +39,55 @@ class Frontend {
 		}
 			return self::$instance;
 		}
+
+	/**
+	 * Overwrites the mobile content and adds in the src tag
+	 * @param $content
+	 *
+	 * @return mixed
+	 */
+	public function mobile_srcset_tag( $content ) {
+		if ( function_exists( 'has_blocks' ) && has_blocks() ) {
+
+			if ( has_block( 'lsx-blocks/lsx-testimonial' ) ) {
+
+				$div_matches = array();
+				preg_match_all('/<div class="lsx-block-post-grid-image">(.*?)<\/div>/s', $content, $div_matches);
+
+				if ( ! empty( $div_matches ) && ! empty( $div_matches[1] ) ) {
+
+					foreach ( $div_matches[1] as $image_match ) {
+						if ( strpos( $image_match, 'srcset' ) === false ) {
+
+							//Get the iamge URL
+							$current_image_url = false;
+							$image_urls = array();
+							preg_match_all( '@src="([^"]+)"@' , $image_match, $image_urls );
+
+							if ( ! empty( $image_urls ) && isset( $image_urls[1] ) && ! empty( $image_urls[1] ) && isset( $image_urls[1][0] ) ) {
+								$current_image_url = $image_urls[1][0];
+							}
+
+							if ( false !== $current_image_url ) {
+
+								//replace the extension with the mobile size.
+								$mobile_image = $current_image_url;
+								$mobile_image = str_replace( '-600x400.jpg', '-350x230.jpg', $mobile_image );
+								$mobile_image = str_replace( '-600x400.png', '-350x230.png', $mobile_image );
+								$mobile_image = str_replace( '-600x400.jpeg', '-350x230.jpeg', $mobile_image );
+
+								$srcset_html = ' srcset="' . $mobile_image . ' 400w,' . $current_image_url . ' 1024w" sizes="(max-width: 400px) 50vw, 10vw" ';
+
+								$new_image_html = str_replace( '<img', '<img ' . $srcset_html, $image_match );
+								$content = str_replace( $image_match, $new_image_html, $content );
+							}
+						}
+					}
+				}
+				//die();
+			}
+		}
+		return $content;
+	}
 
 }
